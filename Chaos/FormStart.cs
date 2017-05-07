d2e855c64f34e5755488b9328e13d1e8ab59e042
GaladMad <kam.fedio@gmail.com>
2017-05-07 20:50:35 +0200

    dodanie okna z ustawieniami zawierającego kontrolki

---
+++ Chaos/FormStart.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chaos
+{
+    public partial class FormStart : Form
+    {
+        public FormStart()
+        {
+            InitializeComponent();
+        }
+
+        private void FormStart_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bNewGame_Click(object sender, EventArgs e)
+        {
+            this.SetingsPanel.Show();
+        }
+
+        private void buttonsPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void bExitGame_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bLoadGame_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lNumberOfPlayers_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bStart_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tNameOfPlayer_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
